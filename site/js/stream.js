28e4d3bb71fd077d276d8d82af71f6af93ad3633
Maksim Terekhin <neurostep@aboutecho.com>
2012-06-16 23:41:07 +0400

    test requests to twitter + some css rules

--- site/js/stream.js
+++ site/js/stream.js
@@ -4,8 +4,8 @@
 
 $.extend({
 	foldl: function(acc, obj, callback) {
-		$.each(object, function(key, item) {
-			result = callback(item, acc, key);
+		$.each(obj, function(key, item) {
+			var result = callback(item, acc, key);
 			if (result !== undefined) acc = result;
 		});
 		return acc;
@@ -16,8 +16,8 @@ $.extend({
 
 GeoStream = function(config) {
 	if (!(config && config.target)) return;
-	config = $.extend(true, {
-		apiURL: "",
+	this.config = $.extend(true, {
+		apiURL: "http://search.twitter.com/",
 		cdnURL: ""
 	}, config);
 	this.init(config.data);
@@ -26,19 +26,42 @@ GeoStream = function(config) {
 GeoStream.prototype = {
 	constructor: GeoStream,
 	init: function(data) {
-		this.getGeoLocation();
+		this.getGeoLocation(function(data) {
+			this.apiCall("search.json", {
+				q: "",
+				rpp: 100,
+				geocode: data.coords.latitude + "," + data.coords.longitude + ",10mi"
+			}, function(response) {
+				this.config.target.append(this.constructItems(response.results));
+			}.bind(this));
+		}.bind(this));
 	},
 	constructItems: function(items) {
-		return $.foldl($('<div class=""></div'), items, function(item, container) {
-			
+		return $.foldl($('<div class="geo-stream-wrapper"></div'), items, function(item, container) {
+			var row = $('<div class="row">' +
+				'<div class="span6 offset3">' +
+					'<div class="hero-unit">' +
+						'<div class="geo-stream-item">' +
+							'<div class="geo-stream-item-avatar">' +
+								'<img src="'+ item.profile_image_url +'">' +
+							'</div>' +
+							'<div class="geo-stream-item-content">' +
+								'<div class="geo-stream-item-userName">'+ item.from_user_name + ' ( ' + item.from_user + ' )</div>' +
+								'<div class="geo-stream-item-text">'+ item.text + '</div>' +
+							'</div>' +
+							'<div class="geo-stream-clear"></div>' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>').appendTo(container);
 		});
 	},
-	getGeoLocation: function() {
-		geo.getCurrentPosition(function(data) {console.log(data);});
+	getGeoLocation: function(callback) {
+		geo.getCurrentPosition(callback);
 	},
 	apiCall: function(endpoint, params, callback) {
 		var config = this.config;
-		$.get(config.apiURL, params || {}, callback, "jsonp");
+		$.get(config.apiURL + endpoint, params || {}, callback, "jsonp");
 	}
 };	
 })(jQuery, navigator.geolocation);
