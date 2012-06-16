dd515a886216f9d3aefe58746c91c377bf94f79d
Neurostep <drumsuck@gmail.com>
2012-06-16 17:56:40 +0400

    initialize js file(s)

---
+++ js/stream.js
@@ -0,0 +1,12 @@
+(function($) {
+var GeoStream = function(config) {
+	if (!(config && config.target)) return;
+	this.init(config.data);
+};
+
+GeoStream.prototype = {
+	constructor: GeoStream,
+	init: function(data) {
+		
+	}
+})(jQuery);
