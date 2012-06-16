53fc0e8ed809cab1a3a0315eb2c3597bd869ad67
Neurostep <drumsuck@gmail.com>
2012-06-16 19:42:40 +0400

    Added geolocation support

--- js/stream.js
+++ js/stream.js
@@ -1,12 +1,44 @@
-(function($) {
-var GeoStream = function(config) {
+(function($, geo) {
+
+/* Utilities */
+
+$.extend({
+	foldl: function(acc, obj, callback) {
+		$.each(object, function(key, item) {
+			result = callback(item, acc, key);
+			if (result !== undefined) acc = result;
+		});
+		return acc;
+	}
+});
+
+/* /Utilities  */
+
+GeoStream = function(config) {
 	if (!(config && config.target)) return;
+	config = $.extend(true, {
+		apiURL: "",
+		cdnURL: ""
+	}, config);
 	this.init(config.data);
 };
 
 GeoStream.prototype = {
 	constructor: GeoStream,
 	init: function(data) {
-		
+		this.getGeoLocation();
+	},
+	constructItems: function(items) {
+		return $.foldl($('<div class=""></div'), items, function(item, container) {
+			
+		});
+	},
+	getGeoLocation: function() {
+		geo.getCurrentPosition(function(data) {console.log(data);});
+	},
+	apiCall: function(endpoint, params, callback) {
+		var config = this.config;
+		$.get(config.apiURL, params || {}, callback, "jsonp");
 	}
-})(jQuery);
+};	
+})(jQuery, navigator.geolocation);
